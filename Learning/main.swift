32491e2a6ef232bcad1a2cb52c1b9eb9ef98fe74
Rajesh M <rajeshm79@outlook.com>
2018-01-16 22:41:43 +0530

    commit

--- Learning/main.swift
+++ Learning/main.swift
@@ -8,5 +8,42 @@
 
 import Foundation
 
-print("Hello, World!")
+enum MycalcError: ErrorType {
+    
+    case DividByZero
+    
+    case DividByOne
+}
+
+func divide(firstvalue first: Int, secondValue second: Int) throws -> Double {
+    
+    if second == 0 {
+        
+       throw MycalcError.DividByZero
+    } else if second == 1 {
+        
+        throw MycalcError.DividByOne
+    }
+    
+    
+    return Double(first) / Double(second)
+}
+
+do {
+
+var result = try divide(firstvalue: 5, secondValue: 1)
+
+print("the result is-------->", result)
+
+
+} catch MycalcError.DividByZero {
+    
+    print("You can't divide by zero")
+}
+
+catch MycalcError.DividByOne {
+    
+    print("You can't divide by one also")
+}
+
 
